7ebb10f36a3a78f056962a09bcd245072b7ce3df
Kevin Tovar <kevintovar@esimez.mx>
2019-09-20 09:33:05 -0500

    Add dot env

--- upload_csv.py
+++ upload_csv.py
@@ -1,4 +1,9 @@
 import pandas
+import os
+from graphqlclient import GraphQLClient
+from dotenv import load_dotenv
+
+client = GraphQLClient(os.getenv("GRAPHQL_URL"))
+client.inject_token("JWT {}".format(os.getenv("JWT_TOKEN")))
 
 shirts = pandas.read_csv("aditivo-playeras-caballero.csv")
-print(shirts)
